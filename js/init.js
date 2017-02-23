0a30de5e583d313646bdd5cdac9b129bca2f8072
MaclaineSabino <emacsabino@hotmail.com>
2017-02-23 11:15:07 -0300

    Menu flutuate
    Implementei um menu flutuante usando CSS e JQUERY, para que ao clicar em
    contato o menu apareça centralmente, clicando em fechar a página
    principal volta a opacidade normal

--- js/init.js
+++ js/init.js
@@ -2,8 +2,53 @@
   $(function(){
 
     $('.button-collapse').sideNav();
-
+    
+    
+    
+    
+    $('#botao-nibble').click(function(){  //efeito formulario flutuante
+    
+    	
+		$('#formulario-nibble').css('position','fixed');
+		$('#formulario-nibble').css('display','block');
+		$('#formulario-nibble').css('z-index','1000');
+		$('#botao-nibble-fechar').css('z-index','1200');
+		var largura = $('body').width();
+		
+	
+		var larguraform = $('#formulario-nibble').width();
+	
+		var left = (largura-larguraform)/2;
+	
+		$('#formulario-nibble ~ nav').css('opacity','0.2');
+		$('#formulario-nibble ~ div').css('opacity','0.2');
+		$('#formulario-nibble ~ footer').css('opacity','0.2');
+		
+		
+		$('#formulario-nibble').css('margin-top','100px');
+		$('#formulario-nibble').css('margin-left',left+'px');
+		$('#formulario-nibble').css('display','block');
+		
+		$('#botao-nibble-fechar').click(function(e){
+			$('#formulario-nibble').css('display','none');
+			$('#formulario-nibble ~ div').css('opacity','1.0');
+			$('#formulario-nibble ~ nav').css('opacity','1.0');
+		$('#formulario-nibble ~ div').css('opacity','1.0');
+		$('#formulario-nibble ~ footer').css('opacity','1.0');
+			
+			
+		});
+	
+		});  //fim formulario flutuante
+		
+		
+		
+		
+  
   }); // end of document ready
   $('.carousel').carousel();
+  
+  
+
 
 })(jQuery); // end of jQuery name space
