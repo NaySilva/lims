24c068a15034c48c35ab8244fa0676a59d437d2e
MaclaineSabino <emacsabino@hotmail.com>
2017-02-24 11:00:16 -0300

    Alterações para versão inicial
    Foi criado a lista de mebros com os devidos efeitos
    Foi criado efeitos par a lista de publicações
    Foi acrescentado lista de navegação

--- js/init.js
+++ js/init.js
@@ -3,10 +3,120 @@
 
     $('.button-collapse').sideNav();
     
+    $('#btn_membros').click(function(){
+    	
+    	$('#index-banner').css('display','none');
+    	$('.section').css('display','none');
+    	$('#nibble_lista').css('display','none');
+    	$('#nibble_membros').css('display','block');
+    	$('#pj_projetos').css('display','none');
+    	
+    });
+    
+      $('#btn_home').click(function(){
+    	
+    	$('#index-banner').css('display','block');
+    	$('.section').css('display','block');
+    	$('#nibble_membros').css('display','none');
+    	$('#nibble_lista').css('display','none');
+    	$('#pj_projetos').css('display','none');
+    });
+     $('#btn_publicacoes').click(function(){
+    	
+    	$('#index-banner').css('display','none');
+    	$('.section').css('display','none');
+    	$('#nibble_membros').css('display','none');
+    	$('#nibble_lista').css('display','block');
+    	$('#pj_projetos').css('display','none');
+    });
+    $('#btn_projetos').click(function(){
+    	$('#index-banner').css('display','none');
+    	$('.section').css('display','none');
+    	$('#nibble_membros').css('display','none');
+    	$('#nibble_lista').css('display','none');
+    	$('#pj_projetos').css('display','block');
+    });
     
     
     
-    $('#botao-nibble').click(function(){  //efeito formulario flutuante
+    
+    $('#pj_projetos #lis_projetos li').hover(function(e){ //efeito sobre a lista de projetos
+    	
+    	$(this).css('background-color','#D9EAF7');
+    	
+    	
+    },
+    function(e){
+    	
+    	$(this).css('background-color','#FFFFFF');
+    	
+    	
+    });//fim efeito sobre lista projetos
+    
+    
+    
+    
+    $('#nibble_membros li').hover(function(e){ //efeito sobre a lista de membros
+    	
+    	$(this).css('background-color','#D9EAF7');
+    	
+    	
+    	
+    },
+    function(e){
+    	
+    	$(this).css('background-color','#FFFFFF');
+    	
+    	
+    });//fim efeito sobre lista de membros
+    
+    
+    
+    
+    
+    $('#nibble_membros li img').hover(
+    function(e){
+    	
+    	$(this).css('height','100px');
+    	$(this).css('width','100px');
+    	
+    }, 
+    function(e){
+    	
+    		$(this).css('height','45px');
+    	$(this).css('width','45px');
+    	
+    	
+    });
+    
+    
+    
+    
+    
+    var len=$('.conteudo').text().length; //efeito sobre a listagem de publicações
+        if(len>80)
+        {
+           // $('.conteudo').text($('.conteudo').text().substr(0,90)+'...');
+           var query = $('.conteudo').text().split(" ", 12);
+                query.push('...');
+                var res = query.join(' ');
+            $('.conteudo').text(res);
+        }
+	
+	
+	$('#nibble_lista ul .listas').hover(function(e){
+		$(this).css('box-shadow','10px 6px 11px 1px rgba(0,0,0,0.75)');
+	},
+	function(e){
+		
+		$(this).css('box-shadow','0px 0px 34px -14px rgba(0,0,0,0.75)');
+		
+	});//fim do efeito da listagem de publicações
+    
+    
+    
+    
+    $('#btn_contato').click(function(){  //efeito formulario flutuante
     
     	
 		$('#formulario-nibble').css('position','fixed');
@@ -20,9 +130,9 @@
 	
 		var left = (largura-larguraform)/2;
 	
-		$('#formulario-nibble ~ nav').css('opacity','0.1');
-		$('#formulario-nibble ~ div').css('opacity','0.1');
-		$('#formulario-nibble ~ footer').css('opacity','0.1');
+		$('#formulario-nibble ~ nav').css('opacity','0.2');
+		$('#formulario-nibble ~ div').css('opacity','0.2');
+		$('#formulario-nibble ~ footer').css('opacity','0.2');
 		
 		
 		$('#formulario-nibble').css('margin-top','100px');
